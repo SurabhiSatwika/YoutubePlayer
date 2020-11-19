2fcdb36113cfdec5aca1208bd7652f634c968b10
SaiBindu <surabhisaibindu@gmail.com>
2020-11-19 23:42:21 +0530

    YoutubePlayer

---
+++ app/src/main/java/com/example/youtubetest/PlayerConfig.java
@@ -0,0 +1,8 @@
+package com.example.youtubetest;
+
+public class PlayerConfig {
+    PlayerConfig(){
+
+    }
+    public static final String API_KEY="AIzaSyBGlF9G6YiW8JHc9n6Cjf_oLfrIA86pcyU";
+}
---
+++ app/src/main/java/com/example/youtubetest/YouTubeBaseActivity.java
@@ -0,0 +1,4 @@
+package com.example.youtubetest;
+
+public class YouTubeBaseActivity {
+}
